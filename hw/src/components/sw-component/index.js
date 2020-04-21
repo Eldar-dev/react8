7566a208cb804906bc9d675cc45a4ab1d8f08722
Eldar <ilona0179@mail.ru>
2020-04-21 19:40:16 +0300

    start

---
+++ hw/src/components/sw-component/index.js
@@ -0,0 +1,10 @@
+import { PersonPage, PlanetPage, StarshipPage } from "./item-lists";
+import { PersonDetails, PalnetDetails, StarshipsDetails } from "./details";
+export {
+  PersonPage,
+  PlanetPage,
+  StarshipPage,
+  PersonDetails,
+  PalnetDetails,
+  StarshipsDetails,
+};
